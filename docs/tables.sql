0fbd3639ff694044914223b3a2be4e3a3d7f9c1c
zhanghailin <zhanghailin@jiagouyun.com>
2019-10-14 18:32:34 +0800

    增加broker模块

--- docs/tables.sql
+++ docs/tables.sql
@@ -7,5 +7,5 @@ create table `user` (
     password varchar (60) comment '密码',
     create_time datetime comment '创建时间',
     update_time datetime comment '修改时间',
-    status int comment default 1 comment '状态'
+    status int default 1 comment '状态'
 )engine = innodb default character set = utf8;
